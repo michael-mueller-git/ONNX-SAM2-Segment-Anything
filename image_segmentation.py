496ad8ed917c656f5b1a0a9b77853e5cca3eda7d
nix <nix@local>
2024-08-01 20:28:38 +0200

    use simple cv2 mouse input

--- image_segmentation.py
+++ image_segmentation.py
@@ -19,18 +19,27 @@ sam2 = SAM2Image(encoder_model_path, decoder_model_path)
 
 sam2.set_image(img)
 
-point_coords = [np.array([[420, 440]]), np.array([[360, 275], [370, 210]]), np.array([[810, 440]]),
-                np.array([[920, 314]])]
-point_labels = [np.array([1]), np.array([1, 1]), np.array([1]), np.array([1])]
+masks = None
+label_id = 0
 
-for label_id, (point_coord, point_label) in enumerate(zip(point_coords, point_labels)):
-    for i in range(point_label.shape[0]):
-        sam2.add_point((point_coord[i][0], point_coord[i][1]), point_label[i], label_id)
+def mouse_callback(event, x, y, flags, param):
+    global label_id, masks
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(f"Left mouse button clicked at ({x}, {y})")
+        sam2.add_point((x, y), 1, label_id)
+        label_id += 1
+        masks = sam2.update_mask(select_best=True)
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        print(f"Right mouse button clicked at ({x}, {y})")
 
-    masks = sam2.update_mask(select_best=True)
+window_name = "image"
+cv2.namedWindow(window_name)
+cv2.setMouseCallback(window_name, mouse_callback)
 
-    masked_img = draw_masks(img, masks)
-
-    cv2.imshow("masked_img", masked_img)
-    if cv2.waitKey(1000) & 0xFF == ord('q'):
+while True:
+    masked_img = draw_masks(img, masks) if masks is not None else img
+    cv2.imshow(window_name, masked_img)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
         break
+
+print("exit")
